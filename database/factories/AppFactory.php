59b885c8de13fad59fa8341c741ac956c6bc386c
artemkoval <artem.koval@infopulse.com>
2021-11-17 14:27:01 +0200

    Drop foreign migration

--- database/factories/AppFactory.php
+++ database/factories/AppFactory.php
@@ -2,6 +2,7 @@
 
 namespace PlureGames\PlureApps\Database\Factories;
 
+use App\Services\Micros;
 use PlureGames\PlureApps\Models\App;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
@@ -15,6 +16,8 @@ class AppFactory extends Factory
         return [
             'name' => $this->faker->name,
             'url' => $this->faker->url,
+            'currency_id' => 2,
+            'currency_amount' => Micros::to($this->faker->randomDigit()),
             'updated_at' => Carbon::now(),
             'created_at' => Carbon::now(),
         ];
--- database/migrations/2021_11_17_095136_add_user_start_balance_fields_to_apps_table.php
+++ database/migrations/2021_11_17_095136_add_user_start_balance_fields_to_apps_table.php
@@ -14,11 +14,15 @@ class AddUserStartBalanceFieldsToAppsTable extends Migration
     public function up()
     {
         \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Schema::table('apps', function (Blueprint $table) {
-            $table->unsignedBigInteger('currency_id')->default(\App\Models\Currency::where('currency', 'diamonds')->first()->id);
-            $table->foreign('currency_id')->references('id')->on('currencies');
-            $table->bigInteger('currency_amount')->default(200000000);
-        });
+        Schema::table(
+            'apps',
+            function (Blueprint $table) {
+                $table->unsignedBigInteger('currency_id')->default(
+                    optional(\App\Models\Currency::where('currency', 'diamonds')->first())->id ?? 1
+                );
+                $table->bigInteger('currency_amount')->default(200000000);
+            }
+        );
         \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 
@@ -30,11 +34,13 @@ class AddUserStartBalanceFieldsToAppsTable extends Migration
     public function down()
     {
         \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Schema::table('apps', function (Blueprint $table) {
-            $table->dropForeign('apps_currency_id_foreign');
-            $table->dropColumn('currency_id');
-            $table->dropColumn('currency_amount');
-        });
+        Schema::table(
+            'apps',
+            function (Blueprint $table) {
+                $table->dropColumn('currency_id');
+                $table->dropColumn('currency_amount');
+            }
+        );
         \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
