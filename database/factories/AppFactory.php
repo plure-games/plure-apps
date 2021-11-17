a2923d5077d5bb6e84409509dd4a731e1bfa6031
artemkoval <artem.koval@infopulse.com>
2021-11-17 15:45:54 +0200

    Remove micros

--- database/factories/AppFactory.php
+++ database/factories/AppFactory.php
@@ -2,7 +2,6 @@
 
 namespace PlureGames\PlureApps\Database\Factories;
 
-use App\Services\Micros;
 use PlureGames\PlureApps\Models\App;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
@@ -17,7 +16,7 @@ class AppFactory extends Factory
             'name' => $this->faker->name,
             'url' => $this->faker->url,
             'currency_id' => 2,
-            'currency_amount' => Micros::to($this->faker->randomDigit()),
+            'currency_amount' => 200000000,
             'updated_at' => Carbon::now(),
             'created_at' => Carbon::now(),
         ];
